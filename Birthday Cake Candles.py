7eb23f75cf5259831aa6787b42a5c04d62676bf3
Himanshu Hooda <himanshuhooda2@gmail.com>
2022-09-01 23:01:13 -0400

    Birthday Cake Candles

---
+++ Birthday Cake Candles.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    count = 0
+    number = 0
+    for i in candles:
+        if i > number:
+            number = i
+            # count += 1
+    for j in candles:
+        if j == number:
+            count += 1
+    return count
+        
+        
+        
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
