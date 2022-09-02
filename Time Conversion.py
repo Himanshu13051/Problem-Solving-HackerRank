24b1af9210dae5564fb0c039c45fc2f0ca4d3c21
Himanshu Hooda <himanshuhooda2@gmail.com>
2022-09-01 23:40:25 -0400

    Time Conversion

---
+++ Time Conversion.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    
+    if s[8] == "A":
+        if s[:2] == "12":
+            s = '00'+ s[2:8]
+        
+        else:
+            s = s[:8]
+        
+        
+        
+    elif s[8] == "P":
+        time = int(s[:2])
+        if time != 12:
+            time += 12
+            s = str(time) + s[2:8]
+        
+        else:
+            s = s[:8]
+
+    return s
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
